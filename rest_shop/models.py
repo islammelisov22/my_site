2ddd8ffd3976740dce41cba9ea6cd2c671cf149f
min_min4ik <nurtilek.mansurov6@gmail.com>
2021-04-08 20:49:01 +0300

    oauth

--- rest_shop/models.py
+++ rest_shop/models.py
@@ -5,7 +5,11 @@ from django.db import models
 class Kit(models.Model):
     total_before = models.CharField(max_length=100)
     total_after = models.CharField(max_length=100)
-    items = models.CharField(max_length=100, verbose_name='Введите товар', help_text='Товар')
-    percent = models.ManyToManyField(validators=[MaxValueValidator])
+    items = models.ManyToManyField(max_length=250, verbose_name='Введите товар', help_text='Товар')
+    percent = models.CharField(validators=[MaxValueValidator])
+
+
+def __str__(self):
+    return self.title
 
 # Create your models here.
--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from shop import views
 
+from shop import views
 
 urlpatterns = [
     path('add/', views.add_dish, name="add"),
-    path('', views.view_dishes, name="index"),
+    path('index/', views.view_dishes, name="index"),
     path('edit/', views.edit_profile, name="edit"),
     path('login/', views.log_in, name="login"),
     path('logout/', views.log_out, name="logout"),
     path('register/', views.register, name="register"),
-
 ]
